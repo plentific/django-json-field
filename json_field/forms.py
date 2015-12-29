5fd9af03236da0e9ed0496d51b3f267efee55aef
Matti John <matti.john@gmail.com>
2015-12-29 13:07:15 +0000

    Fix importing django.forms.util

--- json_field/forms.py
+++ json_field/forms.py
@@ -2,7 +2,12 @@ try:
     import json
 except ImportError:  # python < 2.6
     from django.utils import simplejson as json
-from django.forms import fields, util
+try:
+    from django.forms import util
+except ImportError:  # django >= 1.9
+    from django.forms import utils as util
+
+from django.forms import fields
 
 import datetime
 from decimal import Decimal
